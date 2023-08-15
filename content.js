916619d16839cbbd2963dc1f5157fb7de72c3235
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-15 16:02:37 +0530

    update

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  analyzeAndStoreData();
+}
+
 function setStorageData(data) {
   chrome.runtime.sendMessage({ target: "setStorageData", data: data });
 }
@@ -5,26 +9,22 @@ function setStorageData(data) {
 async function analyzeAndStoreData() {
   const url = document.URL;
   let dataToStore = {};
-  if (["jpg", "png", ".jpeg", ".webp"].some((ext) => url.includes(ext))) {
+  if ([".jpg", ".png", ".jpeg", ".webp"].some(ext => url.includes(ext))) {
     const data = await processImage(url);
     dataToStore = {
-      date: new Date().toLocaleString("en-Us", { timeZone: "Asia/Kolkata" }),
+      date: new Date().toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'}),
       score: data.averageScore,
-      status: data.isContentInappropriate
-        ? "Inappropriate"
-        : "Not Inappropriate",
-      url,
+      status: data.isContentInappropriate ? "Inappropriate" : "Not Inappropriate",
+      url
     };
   } else {
     const content = document.body.innerText;
     const data = await analyzeContent(content);
     dataToStore = {
-      date: new Date().toLocaleString("en-Us", { timeZone: "Asia/Kolkata" }),
+      date: new Date().toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'}),
       score: data.averageScore,
-      status: data.isContentInappropriate
-        ? "Inappropriate"
-        : "Not Inappropriate",
-      url,
+      status: data.isContentInappropriate ? "Inappropriate" : "Not Inappropriate",
+      url
     };
   }
   setStorageData(dataToStore);
--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    chrome.runtime.sendMessage({ target: "getStorageData" }, response => {
-      console.log('Script.js: ', response);
-      if (response.length > 0) {
-        let strHtml = "";
-        response.forEach(data => {
-          strHtml += `<tr><td>${data.date}</td><td>${data.score}</td><td style="text-align: center">${data.status==="Inappropriate"?"⚠️":"✅"}</td><td>${data.url}</td></tr>`;
-        });
-        document.getElementById("storeDataBody").innerHTML = strHtml;
-      }
-    });
+  chrome.runtime.sendMessage({ target: "getStorageData" }, response => {
+    console.log('Script.js: ', response);
+    if (response.length > 0) {
+      let strHtml = "";
+      response.forEach(data => {
+        strHtml += `<tr><td>${data.date}</td><td>${data.score}</td><td style="text-align: center">${data.status==="Inappropriate"?"⚠️":"✅"}</td><td>${data.url}</td></tr>`;
+      });
+      document.getElementById("storeDataBody").innerHTML = strHtml;
+    }
   });
-  
+});
