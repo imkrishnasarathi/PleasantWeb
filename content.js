6d3b0bcd560e24a7115c1d1742ec14c8e78d14b3
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-15 15:35:03 +0530

    create analyzeAndStoreData function

--- content.js
+++ content.js
@@ -1,3 +1,31 @@
 function setStorageData(data) {
-    chrome.runtime.sendMessage({ target: "setStorageData", data: data });
+  chrome.runtime.sendMessage({ target: "setStorageData", data: data });
+}
+
+async function analyzeAndStoreData() {
+  const url = document.URL;
+  let dataToStore = {};
+  if (["jpg", "png", ".jpeg", ".webp"].some((ext) => url.includes(ext))) {
+    const data = await processImage(url);
+    dataToStore = {
+      date: new Date().toLocaleString("en-Us", { timeZone: "Asia/Kolkata" }),
+      score: data.averageScore,
+      status: data.isContentInappropriate
+        ? "Inappropriate"
+        : "Not Inappropriate",
+      url,
+    };
+  } else {
+    const content = document.body.innerText;
+    const data = await analyzeContent(content);
+    dataToStore = {
+      date: new Date().toLocaleString("en-Us", { timeZone: "Asia/Kolkata" }),
+      score: data.averageScore,
+      status: data.isContentInappropriate
+        ? "Inappropriate"
+        : "Not Inappropriate",
+      url,
+    };
+  }
+  setStorageData(dataToStore);
 }
