2da08c110ed60481e9df2ae2bd808c176b5e1422
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-16 13:41:21 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  chrome.runtime.sendMessage({ target: "getStorageData" }, response => {
-    console.log('Script.js: ', response);
-    if (response.length > 0) {
-      let strHtml = "";
-      response.forEach(data => {
-        strHtml += `<tr><td>${data.date}</td><td>${data.score}</td><td style="text-align: center">${data.status==="Inappropriate"?"⚠️":"✅"}</td><td>${data.url}</td></tr>`;
-      });
-      document.getElementById("storeDataBody").innerHTML = strHtml;
-    }
-  });
-});
+// It will update the extension dashboard
