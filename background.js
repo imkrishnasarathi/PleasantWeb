74c2a01e88ab86ce91fee42c5c463922607b2ea2
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-20 13:20:51 +0530

    act with the local storage based on the message received

--- background.js
+++ background.js
@@ -2,4 +2,22 @@
 
 chrome.runtime.onMessage.addListener((message, sender,sendResponse)=> {
     console.log("Message received in background.js:", message);
+    const target = message.target;
+    switch (target) {
+        case "setStorageData":
+          setStorageData(message.data);
+          break;
+        case "getStorageData":
+          getStorageData().then(returnData => {
+            console.log("Sending data from background.js:", returnData);
+            sendResponse(returnData);
+          }).catch(error => {
+            console.error(error);
+            sendResponse([]);
+          });
+          break;
+        case "setTheme":
+          setTheme(message.theme)
+      }
+      return true;
 })
