2349d30e4f5ae58a4293252e38139987c4448562
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-20 13:19:00 +0530

    log the message to the console when received in background.js

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
 //Listen for messages from the content script and perform actions with the local storage.
+
+chrome.runtime.onMessage.addListener((message, sender,sendResponse)=> {
+    console.log("Message received in background.js:", message);
+})
