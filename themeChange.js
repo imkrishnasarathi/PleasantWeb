1779bfa1a97393f4cf2e7d98220bf9a6e3ccf049
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-22 15:09:50 +0530

    Update themeChange.js

--- themeChange.js
+++ themeChange.js
@@ -1,2 +1,13 @@
 //responsible for changing the theme of the page
 function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+  
+    const themeToggle = document.querySelector('.theme-toggle');
+    const isThemeChecked = themeToggle.checked;
+  
+    chrome.storage.local.set({ theme: newTheme, themeChecked: isThemeChecked }, function() {
+      console.log('Theme and state saved:', newTheme, isThemeChecked);
+    });
+  }
