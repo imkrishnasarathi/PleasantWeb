94c592614ffcec4b109d4ef434507c3a98e3b818
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-22 12:52:41 +0530

    Update content.js

--- content.js
+++ content.js
@@ -6,6 +6,13 @@ function setStorageData(data) {
   chrome.runtime.sendMessage({ target: "setStorageData", data: data });
 }
 
+function getTagsWithText(element) {
+  return Array.from(element.getElementsByTagName('*')).filter(tag => {
+    return tag.childNodes.length === 1 && tag.firstChild.nodeType === Node.TEXT_NODE;
+  });
+}
+const tagsWithText = getTagsWithText(document.body);
+
 async function analyzeAndStoreData() {
   const url = document.URL;
   let dataToStore = {};
