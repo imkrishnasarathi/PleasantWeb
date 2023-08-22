02f0a594a7a6ea1519b559bb1accfae810d20937
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-22 19:26:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
       if (response.length > 0) {
         let strHtml = "";
         response.forEach(data => {
-          strHtml += `<tr><td>${data.date}</td><td>${data.score}</td><td style="text-align: center">${data.status==="Inappropriate"?"⚠️":"✅"}</td><td>${data.url}</td></tr>`;
+          strHtml += `<tr><td>${data.date}</td><td>${data.score}</td><td style="text-align: center">${data.status==="Inappropriate"?"⚠️":"✅"}</td><td>${(data.url).trim()}</td></tr>`;
         });
         document.getElementById("storeDataBody").innerHTML = strHtml;
       }
