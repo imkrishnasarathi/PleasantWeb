8324cc0202314e51d97f75f858bef9a71a106b1b
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-08-22 15:09:57 +0530

    Update themeChange.js

--- themeChange.js
+++ themeChange.js
@@ -11,3 +11,20 @@ function setTheme() {
       console.log('Theme and state saved:', newTheme, isThemeChecked);
     });
   }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.local.get(['theme', 'themeChecked'], function(data) {
+      const savedTheme = data.theme;
+      const savedThemeChecked = data.themeChecked;
+      if (savedTheme) {
+        const root = document.documentElement;
+        root.className = savedTheme;
+  
+        const themeToggle = document.querySelector('.theme-toggle');
+        themeToggle.checked = savedThemeChecked;
+      }
+    });
+  
+    document.querySelector('.theme-toggle').addEventListener('click', setTheme);
+  });
+  
