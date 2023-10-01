7ac28a3331d50b3382a6dba0c822de51baa19dd4
Mayur <mayurrokade1718@gmail.com>
2023-10-01 20:56:35 +0530

    dark mode added

--- themeChange.js
+++ themeChange.js
@@ -1,32 +1,27 @@
-//responsible for changing the theme of the page
-function setTheme() {
-  const root = document.documentElement;
-  const newTheme = root.className === "dark" ? "light" : "dark";
-  root.className = newTheme;
-
+// Function to toggle the theme
+function toggleTheme() {
+  const htmlElement = document.documentElement;
   const themeToggle = document.querySelector(".theme-toggle");
-  const isThemeChecked = themeToggle.checked;
 
-  chrome.storage.local.set(
-    { theme: newTheme, themeChecked: isThemeChecked },
-    function () {
-      console.log("Theme and state saved:", newTheme, isThemeChecked);
-    }
-  );
+  if (themeToggle.checked) {
+    htmlElement.classList.remove("light");
+    htmlElement.classList.add("dark");
+  } else {
+    htmlElement.classList.remove("dark");
+    htmlElement.classList.add("light");
+  }
+
+  // Save the theme preference in local storage
+  const isDarkMode = htmlElement.classList.contains("dark");
+  localStorage.setItem("darkMode", isDarkMode);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.storage.local.get(["theme", "themeChecked"], function (data) {
-    const savedTheme = data.theme;
-    const savedThemeChecked = data.themeChecked;
-    if (savedTheme) {
-      const root = document.documentElement;
-      root.className = savedTheme;
+  // Check the user's theme preference from local storage
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+  const themeToggle = document.querySelector(".theme-toggle");
+  themeToggle.checked = isDarkMode;
 
-      const themeToggle = document.querySelector(".theme-toggle");
-      themeToggle.checked = savedThemeChecked;
-    }
-  });
-
-  document.querySelector(".theme-toggle").addEventListener("click", setTheme);
+  // Add event listener to the theme toggle switch
+  themeToggle.addEventListener("change", toggleTheme);
 });
