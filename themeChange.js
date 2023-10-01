9be0104d3a18b4ba0a05b270eacbc8769390132c
Mayur <mayurrokade1718@gmail.com>
2023-10-02 00:46:05 +0530

    dark mode working successfully

--- themeChange.js
+++ themeChange.js
@@ -1,4 +1,3 @@
-// Function to toggle the theme
 function toggleTheme() {
   const htmlElement = document.documentElement;
   const themeToggle = document.querySelector(".theme-toggle");
@@ -6,22 +5,24 @@ function toggleTheme() {
   if (themeToggle.checked) {
     htmlElement.classList.remove("light");
     htmlElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   } else {
     htmlElement.classList.remove("dark");
     htmlElement.classList.add("light");
+    localStorage.setItem("theme", "light");
   }
-
-  // Save the theme preference in local storage
-  const isDarkMode = htmlElement.classList.contains("dark");
-  localStorage.setItem("darkMode", isDarkMode);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Check the user's theme preference from local storage
-  const isDarkMode = localStorage.getItem("darkMode") === "true";
+  const savedTheme = localStorage.getItem("theme");
   const themeToggle = document.querySelector(".theme-toggle");
-  themeToggle.checked = isDarkMode;
 
-  // Add event listener to the theme toggle switch
+  if (savedTheme === "dark") {
+    themeToggle.checked = true;
+    toggleTheme(); 
+  } else {
+    themeToggle.checked = false;
+  }
+
   themeToggle.addEventListener("change", toggleTheme);
 });
